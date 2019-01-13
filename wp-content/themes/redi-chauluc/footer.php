2b819e2309e73061dff7e342a70dc8d6e2c35c06
dienkim <trietnk01@gmail.com>
2019-01-13 14:51:02 +0700

    cập nhật

--- wp-content/themes/redi-chauluc/footer.php
+++ wp-content/themes/redi-chauluc/footer.php
@@ -4,6 +4,67 @@
 Footer template
 
 */
+?>
+<div class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="footer-col-1">
+				<h4 class="footer-title">
+					Internationnal Freight Forwarders
+				</h4>
+				<div class="footer-r">
+					R. 467, Van Nam Building, 26 Duong Lang, Dong Da, Hanoi
+				</div>
+				<div class="footer-tel-fax-email">
+					Tel: <a href="javascript:void(0);">(84) 4 35642514/35642515</a>
+				</div>
+				<div class="footer-tel-fax-email">
+					Fax: <a href="javascript:void(0);">(84) 4 35642513</a> - Mobile: <a href="javascript:void(0);">(84) 900405 8885</a>
+				</div>
+				<div class="footer-tel-fax-email">
+					Email: <a href="javascript:void(0);">quynh@worldfreightvn.com</a>
+				</div>
+			</div>
+			<div class="footer-col-2">
+				<h4 class="footer-title">
+					Inquiry
+				</h4>
+				<div class="bf-box">
+					<div class="bf-left">
+						<div style="background-image: url(<?php echo P_IMG.'/wf.png'; ?>);background-size: cover;background-repeat: no-repeat;padding-top: calc(100% / (190 / 40));"></div>
+					</div>
+					<div class="bf-right">
+						Member of the World <br/>
+						Freight Group since <br/>
+						November 200
+					</div>
+					<div class="clr"></div>
+				</div>
+				<div class="business-forum">
+					<div class="business-left">
+						<div style="background-image: url(<?php echo P_IMG.'/bf.png'; ?>);background-size: cover;background-repeat: no-repeat;padding-top: calc(100% / (190 / 40));"></div>
+					</div>
+					<div class="business-right">
+						Vietnam Chamber of <br/>
+						Commerce and <br/>
+						Industry (VCCI)
+					</div>
+					<div class="clr"></div>
+				</div>
+			</div>
+			<div class="footer-col-3">
+				<h4 class="footer-title">
+					Member & Partner
+				</h4>
+				<div class="member-partner-box">
+					
+				</div>
+			</div>
+			<div class="clr"></div>
+		</div>
+	</div>
+</div>
+<?php
 wp_footer();
 ?>
 </body>
