e8d352c4ec5bc6def694c40aaf2e4d5addc07736
dienkim <trietnk01@gmail.com>
2019-01-14 15:04:39 +0700

    cap nhat

--- wp-content/themes/redi-chauluc/template-04-ecommerce-schedule.php
+++ wp-content/themes/redi-chauluc/template-04-ecommerce-schedule.php
@@ -7,10 +7,11 @@ get_header();
 global $zController;
 $productModel=$zController->getModel("/frontend","ProductModel");
 ?>
+<h1 style="display: none;"><?php echo get_bloginfo( 'name','' ); ?></h1>
 <div class="container">
 	<div class="row">
-		<div class="col-lg-8">
-			<h1 class="single-title3">Schedule</h1>
+		<div class="col-lg-9">
+			<h2 class="single-title3">Schedule</h2>
 			<div class="schedule-box">
 				<div class="schedule-slogan">Continue to remain and increase direct consol box service weekly from Hochiminh-Haiphong-Danang-to/via 2 main port Hongkong & Singapore. Further, Everich has opened weekly direct route to these port…</div>
 				<form class="schedule-frm" name="frm-schedule-ecommerce" method="POST">
@@ -70,10 +71,47 @@ $productModel=$zController->getModel("/frontend","ProductModel");
 						</div>
 						<div class="clr"></div>
 					</div>
+					<div class="ecommerce-schedule-submit">
+						<a href="javascript:void(0);">Submit</a>
+					</div>					
 				</form>
 			</div>
+			<div class="ecommerce-schedule-banner" style="background-image: url(<?php echo P_IMG.'/ecommerce-schedule.jpg'; ?>);"></div>
 		</div>
-		<div class="col-lg-4"></div>
+		<div class="col-lg-3">
+			<h2 class="single-title3">Categories</h2>
+			<ul class="categories">
+				<li><a href="javascript:void(0);">Schedule</a></li>
+				<li><a href="javascript:void(0);">Booking online</a></li>
+				<li><a href="javascript:void(0);">Shipping Instruction</a></li>
+				<li><a href="javascript:void(0);">Tracking</a></li>
+				<li><a href="javascript:void(0);">Soa</a></li>
+				<li><a href="javascript:void(0);">Invoice</a></li>			
+			</ul>
+			<form class="your-account-box" method="POST">
+				<h3 class="account-box-title">Manage your account</h3>
+				<div class="account-box-txt">
+					<input type="text" name="username" placeholder="Username" autocomplete="off">
+				</div>
+				<div class="account-box-txt">
+					<input type="text" name="password" placeholder="Password" autocomplete="off">
+				</div>
+				<div class="forgot-password">
+					<a href="javascript:void(0);">Forgot password ?</a>
+				</div>
+				<div class="box-login">
+					<a href="javascript:void(0);">Login</a>
+				</div>
+				<div class="why-register">Why register</div>
+				<div class="instance-access-to">Instant access to:</div>
+				<div class="instance-access-to">- Air & Ocean rate</div>
+				<div class="instance-access-to">- Cargo management & notification</div>
+				<div class="instance-access-to">- Reporting tools</div>
+				<div class="box-register">
+					<a href="javascript:void(0);">Register</a>
+				</div>
+			</form>
+		</div>
 	</div>
 </div>
 <?php
