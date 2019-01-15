b082fd89e3bb363431c298b5abee3471b23e286c
dienkim <trietnk01@gmail.com>
2019-01-15 14:26:38 +0700

    cập nhật

--- wp-content/themes/redi-chauluc/category.php
+++ wp-content/themes/redi-chauluc/category.php
@@ -1,124 +1,84 @@
 <?php 
 get_header();
-global $zController;
-$productModel=$zController->getModel("/frontend","ProductModel");
-$args = $wp_query->query;	
-$args['orderby']='id';
-$args['order']='DESC';		
-$s="";
-if(isset($_POST["s"])){
-	$s=trim($_POST["s"]);
-}
-if(!empty(@$s)){		
-	$args["s"] =@$s;
-}	 
-$wp_query->query($args);
-$the_query=$wp_query;	
-/* start setup pagination */
-$totalItemsPerPage=get_option( 'posts_per_page' );
-$pageRange=3;
-$currentPage=1; 
-if(!empty(@$_POST["filter_page"]))          {
-    $currentPage=@$_POST["filter_page"];  
-}
-$productModel->setWpQuery($the_query);   
-$productModel->setPerpage($totalItemsPerPage);        
-$productModel->prepare_items();               
-$totalItems= $productModel->getTotalItems();               
-$arrPagination=array(
-    "totalItems"=>$totalItems,
-    "totalItemsPerPage"=>$totalItemsPerPage,
-    "pageRange"=>$pageRange,
-    "currentPage"=>$currentPage   
-);    
-$pagination=$zController->getPagination("Pagination",$arrPagination); 
-/* end setup pagination */
-$source_article=array();
-if($the_query->have_posts()){
-	while ($the_query->have_posts()) {
-		$the_query->the_post();
-		$post_id=$the_query->post->ID;                                                                      
-		$permalink=get_the_permalink($post_id);         
-		$title=get_the_title($post_id);
-		$excerpt=wp_trim_words( get_the_excerpt($post_id), 20, '...' ) ;
-		$featured_img=get_the_post_thumbnail_url($post_id, 'full'); 
-		$date_post='';
-		$date_post=get_the_date('d/m/Y',@$post_id);      
-		$row_article["title"]=$title;
-		$row_article["permalink"]=$permalink;
-		$row_article["featured_img"]=$featured_img;
-		$row_article["date_post"]=$date_post;
-		$row_article["excerpt"]=$excerpt;
-		$source_article[]=$row_article;
-	}
-	wp_reset_postdata();	
-}  
 ?>
-<div class="box-news box-news-2">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8">
-				<form name="frm_prduct_lst" method="POST">
-					<input type="hidden" name="filter_page" value="1" />
-					<input type="hidden" name="s" value="<?php echo @$_POST["s"]; ?>" />					
-					<div>
-						<?php 
-						if(count(@$source_article) > 0){
-							foreach ($source_article as $key => $value) { 
-								?>
-								<div class="box-item-news">
-									<div class="row">
-										<div class="col-md-4">
-											<div class="bx-img">
-												<div>
-													<a href="<?php echo @$value['permalink']; ?>">
-														<figure>
-															<img src="<?php echo @$value['featured_img']; ?>" alt="<?php echo @$value['title']; ?>">
-														</figure>													
-													</a>
-												</div>		
-												<div class="box-date"><?php echo @$value['date_post']; ?></div>											
-											</div>		
-										</div>
-										<div class="col-md-8">
-											<h3 class="box-item-news-title"><a href="<?php echo @$value['permalink']; ?>"><?php echo @$value['title']; ?></a></h3>
-											<div class="box-item-news-excerpt">
-												<?php echo @$value['excerpt']; ?>
-											</div>
-											<div class="box-item-readmore">
-												<a href="<?php echo @$value['permalink']; ?>">Xem thêm</a>
-											</div>
-										</div>
-									</div>
+<h1 style="display: none;"><?php echo get_bloginfo( 'name','' ); ?></h1>
+<h2 style="display: none;"><?php echo get_bloginfo( 'name','' ); ?></h2>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-9 col-md-8">
+			<h2 class="single-title3">News & Event</h2>
+			<div class="row">
+				<div class="col-lg-5">
+					<div class="news-event-box">
+						<a href="javascript:void(0)">
+							<figure><div style="background-image: url(<?php echo P_IMG.'/news-event-0.png'; ?>);" class="news-event-img"></div></figure>
+						</a>						
+						<h3 class="news-event-title"><a href="javascript:void(0);">Say Aloha to Hawai</a></h3>
+						<div class="news-event-date-post">October 31, 2018</div>
+						<div class="news-event-excerpt">
+							Famous Pacific Shipping (NZ) is enhancing its intra-Pacifi c Islands freight connections with the launch of a regular direct oceanfreight consolidation
+						</div>
+					</div>					
+				</div>
+				<div class="col-lg-7">
+					<?php 
+					$k=1;
+					for ($i=0; $i < 3; $i++) { 
+						?>
+						<div class="news-event-hw-box">
+							<div class="news-event-img">
+								<a href="javascript:void(0)">
+									<figure><div style="background-image: url(<?php echo P_IMG.'/news-event-'.$k.'.png'; ?>);background-repeat: no-repeat;background-size: cover;padding-top: calc(100% / (370/285))" ></div></figure>
+								</a>	
+							</div>
+							<div class="news-event-info">
+								<h3 class="news-event-title"><a href="javascript:void(0);">Say Aloha to Hawai</a></h3>
+								<div class="news-event-date-post">October 31, 2018</div>
+								<div class="news-event-excerpt">
+									Famous Pacific Shipping (NZ) is enhancing its intra-Pacifi c Islands freight connections with the launch of a regular direct oceanfreight consolidation
 								</div>
-								<?php
-							}
-						}								
-						?>			
-					</div>
-					<div class="hool-pagination">
-						<?php echo @$pagination->showPagination();   ?>	
-					</div>						
-				</form>				
-			</div>
-			<div class="col-md-4">				
-				<?php 
-				$cssclass="rata";				
-				if(is_category( 'blog' )){
-					include get_template_directory() . "/block/block-service.php";
-					$cssclass="ritan";
-				}
-				?>
-				<div class="<?php echo $cssclass; ?>">
-					<?php include get_template_directory() . "/block/block-regsister.php"; ?>
-				</div>					
-				<div class="fanpage-box">
-					<?php include get_template_directory() . "/block/block-fanpage.php"; ?>
+							</div>
+							<div class="clr"></div>
+						</div>
+						<?php
+						$k++;
+					}
+					?>					
 				</div>
 			</div>
+			<hr class="news-event-line" />
+			<?php 
+			$k=1;
+			for ($i=0; $i < 3; $i++) { 
+				?>
+				<div class="news-event-hw-box">
+					<div class="news-event-img">
+						<a href="javascript:void(0)">
+							<figure><div style="background-image: url(<?php echo P_IMG.'/news-event-'.$k.'.png'; ?>);background-repeat: no-repeat;background-size: cover;padding-top: calc(100% / (370/285))" ></div></figure>
+						</a>	
+					</div>
+					<div class="news-event-info">
+						<h3 class="news-event-title"><a href="javascript:void(0);">Say Aloha to Hawai</a></h3>
+						<div class="news-event-date-post">October 31, 2018</div>
+						<div class="news-event-excerpt">
+							Famous Pacific Shipping (NZ) is enhancing its intra-Pacifi c Islands freight connections with the launch of a regular direct oceanfreight consolidation
+						</div>
+					</div>
+					<div class="clr"></div>
+				</div>
+				<?php
+				$k++;
+			}
+			?>		
+		</div>
+		<div class="col-lg-3 col-md-4">
+			<?php include get_template_directory() . '/block/block-categories.php'; ?>	
+			<div class="account-margin-top">
+				<?php include get_template_directory().'/block/block-manage-account.php'; ?>				
+			</div>			
 		</div>
 	</div>
-</div>	
-<?php     
-get_footer();
+</div>
+<?php
+get_footer(); 
 ?>
